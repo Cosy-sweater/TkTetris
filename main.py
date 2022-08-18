02bd5b5d25361702ac9c12df1dcfb980ea5edb4f
Cosy-sweater <vlad.belov37@yandex.ru>
2022-08-18 14:01:15 +0300

    v0.2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
+import pyglet
 
 
 def close_window():
-    if messagebox.askokcancel("Выход", "Выййти из приложения?"):
+    if messagebox.askokcancel("", "Выйти из приложения?"):
         master.destroy()
 
 
@@ -12,7 +13,9 @@ TILE = 45
 GAME_RES = W * TILE, H * TILE
 RES = 750, 940
 FPS = 60
+score = 0
 
+pyglet.font.add_file("font/font.ttf")
 master = Tk()
 master.protocol("WM_DELETE_WINDOW", close_window)
 master.resizable(False, False)
@@ -25,5 +28,19 @@ img_obj1 = PhotoImage(file="images/background_1.png")
 img_obj2 = PhotoImage(file="images/background_2.png")
 canvas.create_image(0, 0, anchor=NW, image=img_obj1)
 canvas.create_image(20, 20, anchor=NW, image=img_obj2)
+
+grid = [canvas.create_rectangle(x * TILE, y * TILE, x * TILE + TILE, y * TILE + TILE) for x in range(W) for y in
+        range(H)]
+for i in grid:
+    canvas.move(i, 20, 20)
+
+canvas.create_text(505, 30, text="TETRIS", fill="#1a9f2c", font=("a_BighausTitul", 55), anchor=NW)
+canvas.create_text(530, 650, text="Record:", fill="#f4e635", font=("a_BighausTitul", 35), anchor=NW)
+canvas.create_text(525, 780, text="Score:", fill="#23d13b", font=("a_BighausTitul", 35), anchor=NW)
+#temp
+record = 0
+
+canvas.create_text(530, 710, text=str(record), fill="#f4e635", font=("a_BighausTitul", 32), anchor=NW)
+canvas.create_text(525, 840, text=str(score), fill="#23d13b", font=("a_BighausTitul", 32), anchor=NW)
 
 mainloop()
