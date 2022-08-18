77966593fe22466cc2c3550692284be810e61490
Cosy-sweater <vlad.belov37@yandex.ru>
2022-08-18 16:10:43 +0300

    v0.3

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tkinter import messagebox
 import pyglet
 import json
 import random
+from random import choice
+from copy import deepcopy
 
 
 def close_window():
@@ -20,12 +22,11 @@ def get_color():
 def game_over():
     global grid
     for i in grid:
-        master.after(1, canvas.itemconfigure(i, fill=get_color()))
+        master.after(1, game_sc.itemconfigure(i, fill=get_color()))
 
 
 with open('app_info.json') as f:
     app_info = json.load(f)
-
 W, H = 10, 20
 TILE = 45
 GAME_RES = W * TILE, H * TILE
@@ -40,24 +41,49 @@ master.protocol("WM_DELETE_WINDOW", close_window)
 master.resizable(False, False)
 master.wm_attributes("-topmost", 1)
 master.title("Tetris" + " " + app_info["version"])
-canvas = Canvas(master, width=RES[0], height=RES[1], bg="red", highlightthickness=0)
-canvas.pack()
+sc = Canvas(master, width=RES[0], height=RES[1], bg="red", highlightthickness=0)
+sc.pack()
+game_sc = Canvas(master, width=GAME_RES[0] + 1, height=GAME_RES[1] + 1, bg="red", highlightthickness=0)
+game_sc.place(x=20, y=20)
 
 img_obj1 = PhotoImage(file="images/background_1.png")
 img_obj2 = PhotoImage(file="images/background_2.png")
-canvas.create_image(0, 0, anchor=NW, image=img_obj1)
-canvas.create_image(20, 20, anchor=NW, image=img_obj2)
+sc.create_image(0, 0, anchor=NW, image=img_obj1)
+game_sc.create_image(0, 0, anchor=NW, image=img_obj2)
 
-grid = [canvas.create_rectangle(x * TILE, y * TILE, x * TILE + TILE, y * TILE + TILE) for x in range(W) for y in
+grid = [game_sc.create_rectangle(x * TILE, y * TILE, x * TILE + TILE, y * TILE + TILE) for x in range(W) for y in
         range(H)]
-for i in grid:
-    canvas.move(i, 20, 20)
 
-canvas.create_text(505, 30, text="TETRIS", fill="#1a9f2c", font=("a_BighausTitul", 55), anchor=NW)
-canvas.create_text(530, 650, text="Record:", fill="#f4e635", font=("a_BighausTitul", 35), anchor=NW)
-canvas.create_text(525, 780, text="Score:", fill="#23d13b", font=("a_BighausTitul", 35), anchor=NW)
+sc.create_text(505, 30, text="TETRIS", fill="#1a9f2c", font=("a_BighausTitul", 55), anchor=NW)
+sc.create_text(530, 650, text="Record:", fill="#f4e635", font=("a_BighausTitul", 35), anchor=NW)
+sc.create_text(525, 780, text="Score:", fill="#23d13b", font=("a_BighausTitul", 35), anchor=NW)
+sc.create_text(530, 710, text=str(record), fill="#f4e635", font=("a_BighausTitul", 32), anchor=NW)
+sc.create_text(525, 840, text=str(score), fill="#23d13b", font=("a_BighausTitul", 32), anchor=NW)
 
-canvas.create_text(530, 710, text=str(record), fill="#f4e635", font=("a_BighausTitul", 32), anchor=NW)
-canvas.create_text(525, 840, text=str(score), fill="#23d13b", font=("a_BighausTitul", 32), anchor=NW)
+figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
+               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
+               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
+               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
+figures = [[(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
+field = [[0 for i in range(W)] for j in range(H)]
+
+figure, next_figure = deepcopy(choice(figures)), deepcopy(choice(figures))
+color, next_color = get_color(), get_color()
+
+for i in range(4):
+    figure_rect_x = figure[i][0] * TILE
+    figure_rect_y = figure[i][1] * TILE
+    game_sc.create_rectangle(figure_rect_x, figure_rect_y, figure_rect_x + TILE, figure_rect_y + TILE,
+                             fill=color)
+
+for i in range(4):
+    figure_rect_x = next_figure[i][0] * TILE + 380
+    figure_rect_y = next_figure[i][1] * TILE + 185
+    sc.create_rectangle(figure_rect_x, figure_rect_y, figure_rect_x + TILE, figure_rect_y + TILE,
+                             fill=next_color)
+
 
 mainloop()
